8ee2c27601ead97c329b29d19baa6213641ee076
Shjung <wjdtngus9@gmail.com>
2023-11-13 23:49:02 +0900

    setting : swagger 설정

---
+++ src/main/java/wanted/budgetmanagement/config/SwaggerConfig.java
@@ -0,0 +1,21 @@
+package wanted.budgetmanagement.config;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.security.SecurityScheme;
+import org.springframework.context.annotation.Configuration;
+
+@OpenAPIDefinition(
+        info = @Info(title = "sns",
+                version = "v1")
+)
+@Configuration
+@SecurityScheme(
+	name = "bearerAuth",
+	type = SecuritySchemeType.HTTP,
+	bearerFormat = "JWT",
+	scheme = "bearer"
+)
+public class SwaggerConfig {
+}
