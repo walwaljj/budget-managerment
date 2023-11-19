3a3f8a1177d7a25f6208373c566593e3ee2c0488
Shjung <wjdtngus9@gmail.com>
2023-11-20 00:29:09 +0900

    docs : swagger에 example data 추가

--- src/main/java/wanted/budgetmanagement/config/security/jwt/JwtRequestDto.java
+++ src/main/java/wanted/budgetmanagement/config/security/jwt/JwtRequestDto.java
@@ -1,10 +1,13 @@
 package wanted.budgetmanagement.config.security.jwt;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Getter;
 
 @Getter
 public class JwtRequestDto {
 
+    @Schema(description = "사용자 계정", example = "user1")
     private String username;
+    @Schema(description = "사용자 비밀번호", example = "123")
     private String password;
 }
--- src/main/java/wanted/budgetmanagement/domain/user/dto/UserRequestDto.java
+++ src/main/java/wanted/budgetmanagement/domain/user/dto/UserRequestDto.java
@@ -1,5 +1,6 @@
 package wanted.budgetmanagement.domain.user.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotEmpty;
 import lombok.Getter;
@@ -9,11 +10,15 @@ import lombok.Getter;
 public class UserRequestDto {
 
     @NotEmpty(message = "사용자ID는 빈 값일 수 없습니다.")
+    @Schema(description = "사용자 계정", example = "user1")
     private String username;
     @NotEmpty(message = "비밀번호는 빈 값일 수 없습니다.")
+    @Schema(description = "사용자 비밀번호", example = "123")
     private String password;
     @NotEmpty(message = "비밀번호는 확인은 빈 값일 수 없습니다.")
+    @Schema(description = "사용자 비밀번호 확인", example = "123")
     private String passwordReChk;
     @Email(message = "이메일 형식이 아닙니다.")
+    @Schema(description = "사용자 이메일", example = "user1@example.com")
     private String email;
 }
