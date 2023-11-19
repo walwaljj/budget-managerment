a2e22535d25ae4e3cfcf288f71d05929dc533527
Shjung <wjdtngus9@gmail.com>
2023-11-19 19:03:57 +0900

    refactor : responseCode 중 필요없는 코드 삭제

--- src/main/java/wanted/budgetmanagement/response/ResponseCode.java
+++ src/main/java/wanted/budgetmanagement/response/ResponseCode.java
@@ -9,39 +9,17 @@ import org.springframework.http.ResponseEntity;
 @ToString
 public enum ResponseCode {
 
-    // ARTICLE
-    ARTICLE_CREATE(HttpStatus.CREATED, "201", "게시글 등록 성공"),
-    ARTICLE_UPDATE(HttpStatus.CREATED, "201", "게시글 수정 성공"),
-    ARTICLE_DELETE(HttpStatus.NO_CONTENT, "201", "게시글 삭제 성공"),
-    ARTICLE_READ(HttpStatus.OK,"200", "게시글 조회 성공"),
-    ARTICLE_LIST_READ(HttpStatus.OK,"200", "게시글 전체 조회 성공"),
-    ARTICLE_LIKES_READ(HttpStatus.OK,"200", "게시글 좋아요 조회 성공"),
-    COMMENT_CREATE(HttpStatus.OK,"200", "댓글 등록 완료"),
-    COMMENT_DELETE(HttpStatus.NO_CONTENT,"201", "댓글 삭제 완료"),
+    // Expenditure
+    Expenditure_CREATE(HttpStatus.CREATED, "201", "지출 등록 성공"),
+    Expenditure_UPDATE(HttpStatus.CREATED, "201", "지출 수정 성공"),
+    Expenditure_DELETE(HttpStatus.NO_CONTENT, "201", "지출 삭제 성공"),
+    Expenditure_READ(HttpStatus.OK,"200", "지출 상세 조회 성공"),
+    Expenditure_LIST_READ(HttpStatus.OK,"200", "지출 전체 조회 성공"),
 
     // USER
     USER_CREATE(HttpStatus.CREATED, "201", "회원 가입 성공"),
     USER_LOGIN(HttpStatus.OK, "200", "로그인 성공"),
-    USER_LOGOUT(HttpStatus.NO_CONTENT, "204", "로그 아웃 성공"),
-    USER_PROFILE_UPDATE(HttpStatus.OK, "200", "사용자 프로필 변경 완료"),
-    USER_PROFILE_READ(HttpStatus.OK, "200", "사용자 프로필 조회 성공"),
-    USER_LIKES_ARTICLE(HttpStatus.OK, "200", "사용자 게시글 좋아요 성공"),
-    USER_LIKES_CANCEL_ARTICLE(HttpStatus.NO_CONTENT, "204", "사용자 게시글 좋아요 취소 성공"),
-    USER_LIKES_READ(HttpStatus.OK, "200", "사용자 좋아요 목록 조회 성공"),
-
-    // FOLLOW
-    USER_FOLLOW_REQUEST(HttpStatus.OK,"201","팔로우 신청 성공"),
-    USER_FOLLOW_CANCEL(HttpStatus.NO_CONTENT,"201","팔로우 취소 완료"),
-    USER_FOLLOW_ARTICLE_LIST(HttpStatus.OK,"200","팔로우한 사용자의 게시글 조회 완료"),
-
-    // FRIEND
-    USER_FRIEND_REQUEST(HttpStatus.OK,"200","친구 신청 완료"),
-    USER_FRIEND_CANCEL(HttpStatus.NO_CONTENT,"201","친구 취소 완료"),
-    USER_FRIEND_REQUEST_ACCEPT(HttpStatus.OK,"200","친구 요청 수락 완료"),
-    USER_FRIEND_REQUEST_LIST(HttpStatus.OK,"200","친구 요청 조회 완료"),
-    USER_FRIEND_REQUEST_REFUSE(HttpStatus.NO_CONTENT,"201","친구 요청 거절 완료"),
-    USER_FRIEND_ARTICLE_LIST(HttpStatus.OK,"200","친구들의 게시글 조회 완료"),
-    USER_FRIENDS_LIST(HttpStatus.OK,"200","친구 목록 조회 완료");
+    USER_LOGOUT(HttpStatus.NO_CONTENT, "204", "로그 아웃 성공");
 
 
     private final HttpStatus httpStatus;
