a0fc7e5a0b34231a01ca6619144e7ddc3db48e9a
Shjung <wjdtngus9@gmail.com>
2023-11-21 20:15:26 +0900

    refactor : dto 변수명 수정

--- src/main/java/wanted/budgetmanagement/domain/expenditure/dto/TotalAmountResponseDto.java
+++ src/main/java/wanted/budgetmanagement/domain/expenditure/dto/TotalAmountResponseDto.java
@@ -1,17 +1,20 @@
 package wanted.budgetmanagement.domain.expenditure.dto;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import wanted.budgetmanagement.domain.Category;
 
 @Getter
 @Builder(toBuilder = true)
 @AllArgsConstructor
+@NoArgsConstructor
+@JsonInclude(JsonInclude.Include.NON_NULL)
 public class TotalAmountResponseDto {
 
     private Category category;
 
-    private Integer integer;
-
+    private Integer amount;
 }
