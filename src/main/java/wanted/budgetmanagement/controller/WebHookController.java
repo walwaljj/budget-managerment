dbcd38551b3b04b9236dd996255a2d6f5f0f110c
Shjung <wjdtngus9@gmail.com>
2023-12-03 23:48:16 +0900

    feat : 웹훅 기능 구현 - 사용자에게 금일 지출을 추천해줌

--- src/main/java/wanted/budgetmanagement/controller/WebHookController.java
+++ src/main/java/wanted/budgetmanagement/controller/WebHookController.java
@@ -1,22 +1,33 @@
 package wanted.budgetmanagement.controller;
 
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import wanted.budgetmanagement.service.WebHookService;
+
+import java.time.LocalDate;
 
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/v1")
+@SecurityRequirement(name = "bearerAuth")
 public class WebHookController {
 
     private final WebHookService webHookService;
 
     @PostMapping("/notification")
-    public String postEvent() {
+    public String postEvent(Authentication auth,
+                            @RequestParam(value = "date", defaultValue = "today") String date) {
 
-        webHookService.sendNotification();
+        if (date.equals("today")) {
+            date = LocalDate.now().toString();
+        }
+
+        webHookService.sendNotification(auth.getName(), LocalDate.parse(date));
         return "이벤트 발생!";
     }
 }
--- src/main/java/wanted/budgetmanagement/service/WebHookService.java
+++ src/main/java/wanted/budgetmanagement/service/WebHookService.java
@@ -1,6 +1,8 @@
 package wanted.budgetmanagement.service;
 
 import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.json.simple.JSONObject;
@@ -10,21 +12,28 @@ import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
+import wanted.budgetmanagement.domain.expenditure.dto.ExpenditureRecommendationResponseDTO;
+
+import java.time.LocalDate;
 
 @Service
 @Slf4j
-@RequiredArgsConstructor
 @SecurityRequirement(name = "bearerAuth")
+@RequiredArgsConstructor
 public class WebHookService {
+
+    private final ExpenditureService expenditureService;
 
     @Value("${discord.webhookURL}")
     private String url;
 
 //    @Scheduled(cron = "0 30 11 * * ?", zone = "Asia/Seoul")
-    public void sendNotification() {
+    public void sendNotification(String username, LocalDate date) {
         JSONObject data = new JSONObject();
 
-        data.put("content", "[알림] 이벤트가 발생하였습니다");
+        String content = getContent(username, date);
+
+        data.put("content", content);
 
         send(data);
     }
@@ -36,5 +45,26 @@ public class WebHookService {
         RestTemplate restTemplate = new RestTemplate();
         HttpEntity<String> entity = new HttpEntity<>(object.toString(), headers);
         restTemplate.postForObject(url, entity, String.class);
+    }
+
+    /**
+     * 사용자에게 알릴 내용을 생성함.
+     * @return 추천 지출
+     */
+    private String getContent(String username, LocalDate date){
+
+        StringBuilder sb = new StringBuilder();
+        sb.append(username + "님! 목표를 위해 "+ date + "( "+ date.getDayOfWeek() +" ) 사용 가능한 적절한 지출을 알려드릴게요!\n");
+
+        ExpenditureRecommendationResponseDTO expenditureRecommendation = expenditureService.expenditureRecommendation(username, date);
+
+        Integer todayBudget = expenditureRecommendation.getTodayBudget();
+
+        sb.append("금일 총 사용 추천 금액 : " + todayBudget.toString()+"\n");
+
+        sb.append(expenditureRecommendation.getMessage());
+
+        sb.append("\n목표를 위해 화이팅~! ദി ᷇ᵕ ᷆ )♡ ");
+        return sb.toString();
     }
 }
